fc77b980c2156438ce5f63a8eb28b1d414f0ca62
Pavithra Purushoth <pavithrapurushothaman19@gmail.com>
2025-01-13 23:14:53 +0100

    Add files via upload

---
+++ doc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.fulda.webshop"},{"l":"com.fulda.webshop.config"},{"l":"com.fulda.webshop.controller"},{"l":"com.fulda.webshop.dto"},{"l":"com.fulda.webshop.facade"},{"l":"com.fulda.webshop.model"},{"l":"com.fulda.webshop.repository"},{"l":"com.fulda.webshop.service"}];updateSearchResults();
